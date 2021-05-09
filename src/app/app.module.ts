b3b6576b1f385ff6f72c6b8e051af3bbb49d021e
RamRavipudi <ram@kahaniya.com>
2021-05-09 16:34:52 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+import { AuditComponent } from './audit';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AuditComponent,
+        AlertComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ src/app/audit/audit.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { Audit } from '@/_models';
+import { AuditService, AuthenticationService } from '@/_services';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator} from '@angular/material/paginator'
+import { ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+@Component({ templateUrl: 'audit.component.html' })
+export class AuditComponent implements OnInit {
+    AUDIT_DATA:Audit[] = [];
+    public currentUser;
+    dateForm:FormGroup;
+    dateFormat:string='12hr';
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private auditService: AuditService,
+        private fb: FormBuilder
+    ) {
+    }
+    displayedColumns: string[] = ['user', 'id', 'LoginTime', 'LogOutTime', 'IP'];
+    adults = new MatTableDataSource(this.AUDIT_DATA);
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+
+
+    ngOnInit() {
+        this.loadAllAudits();
+        this.authenticationService.currentUser.subscribe(x=> this.currentUser =x);
+        this.dateForm = this.fb.group({
+            date:''
+        });
+
+    }
+
+    private loadAllAudits() {
+        this.auditService.getAll().subscribe((res:any)=>{
+            res.forEach(audit=>{
+                this.AUDIT_DATA.push({
+                    user:audit.user,
+                    id: audit._id,
+                    loginTime: audit.loginTime,
+                    logoutTime: audit.logoutTime,
+                    ip: audit.ip
+
+                });
+            });
+            this.adults.data = this.AUDIT_DATA;
+        });
+    }
+
+    public doFilter = (value: string) => {
+        this.adults.filter = value.trim().toLocaleLowerCase();
+      }
+
+      changeDate(){
+        this.dateForm.value.date ? this.dateFormat =this.dateForm.value.date : this.dateFormat;
+        console.log(this.dateForm.value.date);
+      }
+
+    ngAfterViewInit() {
+      this.adults.paginator = this.paginator;
+      this.adults.sort = this.sort;
+
+    }
+}
